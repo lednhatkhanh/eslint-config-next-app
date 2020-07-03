105faea16c1c50ba4796bd929183cbf21259c23f
Khanh Le <lednhatkhanh@gmail.com>
2020-07-03 14:52:19 +0700

    Prepare 0.0.5

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = {
   root: true,
 
-  parser: 'babel-eslint',
-
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
+    'plugin:import/typescript',
     'plugin:jsx-a11y/strict',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
@@ -16,68 +18,32 @@ module.exports = {
     'plugin:jest/all',
   ],
 
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    jest: true,
-    node: true,
-  },
-
   settings: {
     react: {
       version: 'detect',
     },
   },
 
+  plugins: ['@next/eslint-plugin-next'],
+
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
+    warnOnUnsupportedTypeScriptVersion: true,
   },
 
   rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'react/prop-types': 'off',
+    '@typescript-eslint/no-empty-interface': [
+      'error',
+      {
+        allowSingleExtends: true,
+      },
+    ],
+    '@next/next/no-html-link-for-pages': ['error', path.resolve(process.cwd(), './src/pages')],
   },
-
-  overrides: [
-    {
-      files: ['**/*.ts?(x)'],
-
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:import/typescript',
-        'plugin:jsx-a11y/strict',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:@next/eslint-plugin-next/recommended',
-        'plugin:jest/all',
-      ],
-
-      parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
-        warnOnUnsupportedTypeScriptVersion: true,
-      },
-
-      rules: {
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        'react/prop-types': 'off',
-        '@typescript-eslint/no-empty-interface': [
-          'error',
-          {
-            allowSingleExtends: true,
-          },
-        ],
-      },
-    },
-  ],
 };
